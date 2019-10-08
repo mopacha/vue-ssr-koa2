18578054c09feecc974c10ef3e32e1972385d441
unknown <wangpeng04@ppdai.com>
2019-10-08 16:59:52 +0800

    add

--- app.config.js
+++ app.config.js
@@ -7,13 +7,15 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // 主服务启动端口
-  appPort: 8566,
+  appPort: 8656,
   // 代理配置，可支持多个代理，key为前缀，命中后，会把前缀去掉，转发到代理服务器
   proxy: isProd ? constants.prodProxy : constants.proxy,
   // webpack的差异化配置
   webpack: {
     entry: {
-      app: path.join(__dirname, 'src/app.js'), // 入口
+			app: path.join(__dirname, 'src/app.js'), // 入口
+			vendor: ['vue', 'vue-router'], // 拆分框架代码
+			resolveAlias: {} // 自定义Alias设置
     },
   },
 }
--- config/webpack.client.config.js
+++ config/webpack.client.config.js
@@ -1,19 +1,16 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const path = require('path')
-const base = require('./webpack.base.config')
-const SWPrecachePlugin = require('sw-precache-webpack-plugin')
+const base = require('./webpack.base.config')()
+const isProd = process.env.NODE_ENV === 'production'
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 
 const config = merge(base, {
   entry: {
 		app: path.join(process.cwd(), 'src/entry-client.js'),
-  },
-  resolve: {
-    alias: {
-      'create-api': './create-api-client.js'
-    }
-  },
+	},
+	mode: isProd ? 'production' : 'development',
+ 
   plugins: [
     // strip dev-only code in Vue source
     new webpack.DefinePlugin({
@@ -24,35 +21,5 @@ const config = merge(base, {
   ]
 })
 
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    // auto generate service worker
-    new SWPrecachePlugin({
-      cacheId: 'vue-hn',
-      filename: 'service-worker.js',
-      minify: true,
-      dontCacheBustUrlsMatching: /./,
-      staticFileGlobsIgnorePatterns: [/\.map$/, /\.json$/],
-      runtimeCaching: [
-        {
-          urlPattern: '/',
-          handler: 'networkFirst'
-        },
-        {
-          urlPattern: /\/(top|new|show|ask|jobs)/,
-          handler: 'networkFirst'
-        },
-        {
-          urlPattern: '/item/:id',
-          handler: 'networkFirst'
-        },
-        {
-          urlPattern: '/user/:id',
-          handler: 'networkFirst'
-        }
-      ]
-    })
-  )
-}
 
 module.exports = config
