a44a19a39ef3fe53348aa4890e81038abb66fd7b
unknown <wangpeng04@ppdai.com>
2019-10-17 19:23:13 +0800

    tree

--- src/components/tree.vue
+++ src/components/tree.vue
@@ -7,6 +7,13 @@
                :modal="false"
                :visible="true">
       <el-form>
+        <el-form-item label="拨打编号"
+                      label-width="120px">
+          <el-input v-model="version"
+                    name="version"
+                    :disabled="true">
+          </el-input>
+        </el-form-item>
         <el-form-item label="范围"
                       label-width="120px">
           <el-select class="value"
@@ -53,6 +60,7 @@ import Vue from 'vue'
 export default {
   data() {
     return {
+      version: '',
       range: '',
       rangeOptions: [{
         value: '1',
@@ -93,6 +101,7 @@ export default {
         this.editForm = document.getElementById("editForm")
         this.rangeInput = document.getElementsByName("range")
         this.robotInput = document.getElementsByName("robot")
+        this.versionInput = document.getElementsByName("version")
         this.cancelButton = document.getElementById("cancel")
         this.saveButton = document.getElementById("save")
 
@@ -104,11 +113,10 @@ export default {
 
           node.range = that.rangeInput[0].value
           node.robot = that.robotInput[0].value
+          node.version = that.versionInput[0].value
 
+          console.log(node)
           chart.updateNode(node)
-
-
-          console.log(obj)
 
           //整个树的数据结构
           console.log('整个树的数据结构')
@@ -127,17 +135,21 @@ export default {
 
         this.rangeInput[0].value = node.range
         this.robotInput[0].value = node.robot
+        this.versionInput[0].value = node.version
       }
 
       editForm.prototype.hide = function (showldUpdateTheNode) {
         this.editForm.style.display = "none"
       }
 
+
+      OrgChart.templates.ana.field_0 = '<text class="field_0"  style="font-size: 16px;" fill="#ffffff" x="125" y="30" text-anchor="middle">拨打编号：{val}</text>';
+      OrgChart.templates.ana.field_1 = '<text class="field_1"  style="font-size: 16px;" fill="#ffffff" x="125" y="60" text-anchor="middle">拨打范围：{val}</text>';
+      OrgChart.templates.ana.field_2 = '<text class="field_2"  style="font-size: 16px;" fill="#ffffff" x="125" y="90" text-anchor="middle">机器人：{val}</text>';
       var chart = new OrgChart(document.getElementById("tree"), {
-        template: 'ula',
+        template: 'ana',
         mouseScrool: OrgChart.action.zoom,
         enableSearch: false,
-
         nodeMenu: {
           edit: { text: "编辑" },
           add: { text: "添加" },
@@ -145,14 +157,42 @@ export default {
         },
         editUI: new editForm(),
         nodeBinding: {
-          field_0: "range",
-          field_1: "robot"
+          field_0: "version",
+          field_1: "range",
+          field_2: "robot"
         },
         nodes: [
-          { id: 1, range: "范围2", robot: "机器人A" },
-          { id: 2, pid: 1, range: "范围3", robot: "机器人B" },
-          { id: 3, pid: 1, range: "范围1", robot: "机器人C" }
+          { id: 1, version: '1', range: "范围2", robot: "机器人A" },
+          { id: 2, pid: 1, version: '2.1', range: "范围3", robot: "机器人B" },
+          { id: 3, pid: 1, version: '2.2', range: "范围1", robot: "机器人C" }
         ]
+      })
+
+
+      chart.on('add', function (sender, node) {
+        const parentId = node.pid
+        //遍历寻找父节点version
+        let parentVersion = ''
+        chart.config.nodes.map(i => {
+          if (i.id == parentId) {
+            parentVersion = i.version
+            return
+          }
+        })
+
+        // 父节点有几个孩子
+        let num = 1
+        chart.config.nodes.map(i => {
+          if (i.pid == parentId) {
+            num++
+            return
+          }
+        })
+
+        node.version = parentVersion + '.' + num
+        // your code goes here 
+        // return false; to cancel the operation
+        return true;
       })
     },
   }
